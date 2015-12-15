3ec7bb9a2bfa436e27b516fd1a943f8e9b737ace
Humble Chirammal <hchiramm@redhat.com>
2015-12-15 23:07:40 +0530

    Introducing options parser
    Signed-off-by: Humble Chirammal <hchiramm@redhat.com>

--- aplo_prov.go
+++ aplo_prov.go
@@ -1,14 +1,62 @@
+
+//
+// Copyright (c) 2015 
+//
+// Author "Humble Chirammal" < hchiramm@redhat.com>
+
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+
 package main
+
 import (
     "fmt"
     "log"
+    "os"
+    "flag"
    // One other dockerclient
     "github.com/fsouza/go-dockerclient"
 )
 
+var (
+    APLO_VERSION = "1.0"
+   // configfile string
+    image string
+    showVersion bool
+
+)
+
+func init() {
+    flag.StringVar(&image, "image", "", "Docker Image Name")
+   // flag.StringVar(&configfile, "config", "", "Configuration file")
+    flag.BoolVar(&showVersion, "version", false, "Show version")
+}
+
 func main() {
     
     fmt.Println("Info: Aplo Provisioner ")
+    
+    flag.Parse()
+    
+    /*
+    if image == " " {
+        fmt.Println("No image provided, working on gluster/gluster-centos")
+        image := "gluster-centos"
+    }
+    */
+
+    fmt.Printf("Docker Image :%s", image)
 
     endpoint := "unix:///var/run/docker.sock"
    
@@ -18,10 +66,25 @@ func main() {
     
     //fmt.Println("Client is :", client)
     
- 
+    if client == nil {
+        fmt.Println("Failed to connect to the Docker Deamon.. exiting")
+        os.Exit(1)
+    }
+    
+    fmt.Println("Connected to Docker Deamon")
+    
 
     imgs, _ := client.ListImages(docker.ListImagesOptions{All: false})
+
+    // If we want to list all the images
     
+    //imgs, _ := client.ListImages(docker.ListImagesOptions{All: true})
+
+    if imgs == nil {
+        fmt.Println("Listimages: exiting")
+        os.Exit(1)
+    }
+
     for _, img := range imgs {
         fmt.Println("ID: ", img.ID)
         fmt.Println("\t RepoTags: ", img.RepoTags)
@@ -40,12 +103,18 @@ func main() {
 
 
     infoenv, _ := client.Info()
+    if infoenv == nil {
+        fmt.Println("Failed to Get Docker Info.. exiting")
+        os.Exit(1)
+    }
     fmt.Println(infoenv)
+
     //fmt.Println(infoenv.Get("OperatingSystem"))
     
     docker_driver := infoenv.Get("Driver")
     if docker_driver != "devicemapper" {
-        fmt.Println("The docker drivers other than the devicemapper is not supported at the moment")
+        fmt.Println("The docker drivers other than the devicemapper is not supported at the moment ..exiting")
+        os.Exit(1  )
     }
 
      
@@ -66,6 +135,7 @@ func main() {
         
 
     }
+    
     fmt.Println("Create a Gluster Container")
 
     b := make(map[string][]docker.PortBinding)
